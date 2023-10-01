0406a14607eb7caafd5c76ce7c2567acda8d5288
Nerses Hepoyan <nerseshepoyan22@gmail.com>
2023-10-01 07:57:10 -0800

    async await

---
+++ MyClass.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace _003_AsyncAwait
+{
+    internal class MyClass
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Operation ThreadId {0}", Environment.CurrentManagedThreadId);
+            Console.WriteLine("Begin");
+            Console.WriteLine("End");
+        }
+
+        public async void OperationAsync()
+        {
+            Console.WriteLine("First part of OperationAsync Thread Id = {0}", Environment.CurrentManagedThreadId);
+            Task task = new (Operation);
+            task.Start();
+
+            Console.WriteLine("First/Second part of OperationAsync Thread Id = {0}", Environment.CurrentManagedThreadId);
+            await task;
+
+            Console.WriteLine("Second part of OperationAsync Thread Id = {0}", Environment.CurrentManagedThreadId);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using _003_AsyncAwait;
+
+Console.WriteLine("General ThreadId {0}", Environment.CurrentManagedThreadId);
+
+MyClass myClass = new();
+
+myClass.OperationAsync();
+Thread.Sleep(5000);
+Console.WriteLine("General ThreadId {0}",Environment.CurrentManagedThreadId);
